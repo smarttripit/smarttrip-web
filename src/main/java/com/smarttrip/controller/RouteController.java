b9e7d6f6f77a25cdb9cf2700a1d468bd6037306c
gaoweibupt <gaoweibupt@163.com>
2015-10-24 20:52:04 +0800

    更新routeCOntroller

--- src/main/java/com/smarttrip/controller/RouteController.java
+++ src/main/java/com/smarttrip/controller/RouteController.java
@@ -1,6 +1,7 @@
 package com.smarttrip.controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -14,8 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.smarttrip.common.Result;
+import com.smarttrip.domain.Region;
 import com.smarttrip.domain.Route;
 import com.smarttrip.domain.RouteTheme;
+import com.smarttrip.service.IRegionService;
 import com.smarttrip.service.IRouteService;
 import com.smarttrip.service.IRouteThemeService;
 import com.smarttrip.service.IThemeService;
@@ -37,6 +40,9 @@ public class RouteController {
 	
 	@Autowired
 	private IThemeService themeService;
+	
+	@Autowired
+	private IRegionService regionService;
 	
 	
 	@RequestMapping("/top3")
@@ -75,7 +81,7 @@ public class RouteController {
 		return result;
 	}
 	
-	@RequestMapping("/conditions")
+	@RequestMapping("/condition")
 	@ResponseBody
 	public Result toRouteInConditions(HttpServletRequest request,Model model){
 		Result result = new Result();
@@ -83,14 +89,28 @@ public class RouteController {
 		String secondRegion =  request.getParameter("secondRegion");
 		String theme = request.getParameter("theme");
 		String periodString = request.getParameter("period");
-//		String[] firstRegions = firstRegion.split(",");
-//		String[] secondRegions = secondRegion.split(",");
-//		String[] themes = theme.split(",");
-//		String[] periods = period.split(",");
 		if (firstRegion == null && secondRegion ==null && theme == null && periodString == null){
 			result.setStatus("failed");
 			return result;
 		}
+		String[] secondRegions = secondRegion.split(",");
+		String[] themes = theme.split(",");
+		String[] periodStrings = periodString.split(",");
+		int[] periods = new int[periodStrings.length];
+		for (int i = 0; i < periods.length; i++){
+			periods[i] = Integer.parseInt(periodStrings[i]);
+		}
+		
+		List<Route> record = new ArrayList<>();
+		for (int i = 0 ; i < secondRegions.length || i == 0; i++){
+			for (int j = 0; j < themes.length || j == 0; j++){
+				for (int k = 0; k < periods.length || k == 0; k++){
+					record.addAll(routeService.selectByConditions(firstRegion, secondRegions[i], themes[i], periods[i]));
+				}	
+			}
+		}
+		
+		
 		int period = Integer.parseInt(periodString);
 		result.setData(routeService.selectByConditions(firstRegion, secondRegion, theme, period));
 		return result;
@@ -110,6 +130,35 @@ public class RouteController {
 		}
 		return result;
 		
+	}
+	
+	@RequestMapping("/conditions")
+	@ResponseBody
+	public Result readRouteByConditions(HttpServletRequest request,Model model){
+		Result result =  new Result();
+		//大地区只能有一个
+		String firstRegion = request.getParameter("firstRegion");
+		String secondRegionString = request.getParameter("secondRegion");
+		String themeString = request.getParameter("theme");
+		String periodString = request.getParameter("period");
+		String pageNumString = request.getParameter("pageNum");
+		int pageNum = Integer.parseInt(pageNumString);
+		String pageSizeString = request.getParameter("pageSize");
+		int pageSize = Integer.parseInt(pageSizeString);
+		String sortField = request.getParameter("sortField");
+		//String[] firstRegion = firstRegionString.split(",");
+		List<String> secondRegion = Arrays.asList(secondRegionString.split(","));
+		List<String> theme = Arrays.asList(themeString.split(","));
+		String[] periodStrings = periodString.split(",");
+		int[] period = new int[periodStrings.length];
+		for (int i = 0; i < periodStrings.length; i++){
+			period[i] = new Integer(Integer.parseInt(periodStrings[i]));
+		}
+		List<String> regionId = regionService.selectRegionId(firstRegion, secondRegion);
+		
+		List<Route> record = routeService.selectByConditions(regionId, theme, period, pageNum, pageSize, sortField);
+		result.setData(record);
+		return result;
 	}
 
 }
